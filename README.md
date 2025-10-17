3fbf8cc1d0e8d7a0af1801f30a00f22bb05086c5
jhony <jhonatansouzameza100@gmail.com>
2025-10-16 23:04:39 -0300

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
+# Responsive Bakery Website
+
+- Responsive Bakery Website Design Using HTML CSS And JavaScript
+- Contains animations when scrolling.
+- Smooth scrolling in each section.
+- Contains an elegant dark theme.
+- Developed first with the Mobile First methodology, then for desktop.
+- Compatible with all mobile devices and with a beautiful and pleasant user interface.
+
+💙 Go to my website to see more projects like this [jhonysouza100.site](https://jhonysouza100.site) 💙 
+
+![preview img](preview.png)
+
 ## 📦 Vanilla HTML Deployment with Docker & Nginx
 Este repositorio contiene un ejemplo simple de cómo desplegar un sitio web estático construido con HTML puro (vanilla HTML) utilizando Docker y Nginx como servidor web.
 
---
+++ src/script.js
@@ -0,0 +1,103 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav_link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav_link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== ADD BLUR HEADER ===============*/
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SHOW CART ===============*/
+const cart = document.getElementById('cart'),
+      cartShop = document.getElementById('cart-shop'),
+      cartClose = document.getElementById('cart-close')
+
+/*===== CART SHOW =====*/
+/* Validate if constant exists */
+if(cartShop){
+    cartShop.addEventListener('click', () =>{
+        cart.classList.add('show-cart')
+    })
+}
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav_menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '40px',
+    opacity: 1,
+    scale: 1.1,
+    duration: 2500,
+    delay: 300,
+    // reset: true // animations repeat
+})
+
+sr.reveal(`.home_data, .about_img, .about_data, .visit_data`)
+sr.reveal(`.home_image, .footer_img-1, .footer_img-2`, { rotate: { z: -15 } })
+sr.reveal(`.home_bread, .about_bread`, { rotate: { z: 15 } })
+sr.reveal(`.home_footer`, { scale: 1, origin: 'bottom' })
+
+sr.reveal(`.new_card:nth-child(1) img`, { rotate: { z: -30, distance: 0 } })
+sr.reveal(`.new_card:nth-child(2) img`, { rotate: { z: 15, distance: 0, delay: 600 } })
+sr.reveal(`.new_card:nth-child(3) img`, { rotate: { z: -30, distance: 0, delay: 900 } })
+
+sr.reveal(`.favorite_card img`, { interval: 100, rotate: { z: 15, distance: 0 } })
+sr.reveal(`.footer_container`, { scale: 1 })
